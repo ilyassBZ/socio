e1b6e82f7c16a61bae1586d7894a794936cb1b8b
ilyass bzitar <bzitarilyass75@gmail.com>
2022-12-16 10:50:29 +0100

    friends widget and all posts

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,40 @@
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import Friend from "components/Friend"
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { setPost } from "state";
+
+
+
+const PostWidget =({
+            postId,
+            postUserId,
+            name,
+            description,
+            location,
+            picturePath,
+            userPicture,
+            likes,
+            comments,
+})=> {
+	const [isComments,setIsComments]=useState(false)
+	 const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+	const loggedInUserId= useSelector((state)=>state.user._id)
+	const isLike= Boolean(likes[loggedInUserId])
+
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
+}
